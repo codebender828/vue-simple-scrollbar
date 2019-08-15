52b078e56cef99b65b33bd32af16b2fba854db31
codebender828 <jbakebwa@163.com>
2019-08-15 14:11:00 +0800

    add: intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="application">
+    <div class="clip">
+      <simple-scrollbar class="test">
+        <p>{{ greeting }}</p>
+      </simple-scrollbar>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleScrollbar from '@/components/simple-scrollbar.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    SimpleScrollbar
+  },
+  data () {
+    return {
+      greeting: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, iure, tempore atque ratione eveniet, aperiam repellat sequi accusamus facilis repudiandae obcaecati. Blanditiis reprehenderit ducimus modi voluptate ea assumenda ut neque, ratione praesentium aut voluptates quis pariatur? Eum neque fuga architecto quasi maxime voluptatum sit vel? Numquam, ullam. Perspiciatis, ipsa non. Illum alias quisquam, aperiam tempora esse at vel? Aperiam eaque beatae suscipit debitis, ducimus unde illo modi minus. Explicabo, sit? Non incidunt ullam reprehenderit eligendi ut corporis omnis vero corrupti odit. Quae quaerat ipsa ut corrupti hic, placeat sapiente temporibus sunt, quibusdam ea at sequi! Quasi, hic soluta. Provident nihil similique, mollitia voluptates sint aperiam asperiores eius! Sit corporis, molestias enim obcaecati quidem velit, quibusdam dolorum tempore, magni ipsam cum accusantium necessitatibus id quas? Perspiciatis suscipit fuga eligendi possimus deserunt. Numquam ratione ex atque voluptas! Vero sequi tenetur '
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+}
+
+.application {
+  background: #efefef;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  .clip {
+    width: 300px;
+    height: 400px;
+    background: #ffffff;
+  }
 }
 </style>
---
+++ src/components/simple-scrollbar.vue
@@ -0,0 +1,86 @@
+import SimpleScrollbar from '@/components/simple-scrollbar.vue';
+<template>
+  <div ref="el" class="v-ss">
+    <slot name="default"></slot>
+  </div>
+</template>
+
+<script>
+import SimpleScrollbar from 'simple-scrollbar'
+
+export default {
+  name: 'SimpleScrollbar',
+  data () {
+    return {
+      simpleScroll: undefined
+    }
+  },
+  async mounted () {
+    await this.$nextTick()
+    this.polyfillIE9()
+    this.simpleScroll = SimpleScrollbar
+    const el = this.$refs.el
+    this.simpleScroll.initEl(el)
+  }
+}
+</script>
+
+<style lang="scss">
+.v-ss {
+  height: inherit;
+  width: inherit;
+}
+
+.ss-wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+  float: left;
+}
+
+.ss-content {
+  height: 100%;
+  width: calc(100% + 18px);
+  padding: 0 18px 0 0;
+  position: relative;
+  overflow: auto;
+  box-sizing: border-box;
+}
+
+.ss-content.rtl {
+  width: calc(100% + 18px);
+  right: auto;
+}
+
+.ss-scroll {
+  position: relative;
+  background: rgba(0, 0, 0, 0.1);
+  width: 9px;
+  border-radius: 4px;
+  top: 0;
+  z-index: 2;
+  cursor: pointer;
+  opacity: 0;
+  transition: opacity 0.25s linear;
+}
+
+.ss-hidden {
+  display: none;
+}
+
+.ss-container:hover .ss-scroll,
+.ss-container:active .ss-scroll {
+  opacity: 1;
+}
+
+.ss-grabbed {
+  -o-user-select: none;
+  -ms-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+</style>
